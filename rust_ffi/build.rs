5c823a8bf606c8c6eac48b4606f2b5d148ea47ee
Ian Ynda-Hummel <ianynda@gmail.com>
2025-02-12 09:56:42 -0500

    Remove legacy stdlib line

--- rust_ffi/build.rs
+++ rust_ffi/build.rs
@@ -6,10 +6,6 @@ fn main() {
     println!("cargo:rustc-link-lib=static=pacparser");
     println!("cargo:rustc-link-lib=static=js");
 
-    //uses vfprint which is no longer in the standard library in MSVC2019
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-lib=static=legacy_stdio_definitions");
-
     let bindings = bindgen::Builder::default()
         .header("includes/pacparser.h")
         .whitelist_function("pacparser_cleanup")
