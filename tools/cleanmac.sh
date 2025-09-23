f20c20ead2224d1a6169ffec4145183481427ea6
pexarkh <40366153+pexarkh@users.noreply.github.com>
2025-09-23 18:22:59 -0400

    DA-6842

---
+++ tools/cleanmac.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+rm -rf \
+ rust_ffi/target/ \
+ src/jsapi_buildstamp \
+ src/libpacparser.1.dylib \
+ src/libpacparser.dylib \
+ src/pacparser.o \
+ src/pymod/pacparser_o_buildstamp \
+ src/spidermonkey/js-buildstamp \
+ src/spidermonkey/js/src/Darwin_DBG.OBJ/ \
+ src/spidermonkey/js/src/jsautocfg.h \
+ src/spidermonkey/libjs.a \
+ /tmp/pacparser_*
